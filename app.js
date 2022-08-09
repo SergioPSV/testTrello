91bb3634d0f1241bdf1a09e2d25621e7eed802eb
Serhii <32548311+SergioPSV@users.noreply.github.com>
2022-08-09 17:00:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ window.TrelloPowerUp.initialize({
             // to refresh you can mix and match between static and dynamic
             title: "Detail Badge",
             text: "Static",
-            color: randomBadgeColor(),
+            color: green,
           },
           {
             // card detail badges (those that appear on the back of cards)
