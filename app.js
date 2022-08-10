e0c048beed2c1ea8c53983263bdba4c4d95cfcfa
Serhii <32548311+SergioPSV@users.noreply.github.com>
2022-08-10 19:32:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ let tags = ["first", "second", "three", "four"];
 var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 
 var onBtnClick = function (t, opts) {
-  console.log('Someone clicked the button');
+  t.alert({message: 'Сохраняем тег...', duration: 2});
 };
 
 window.TrelloPowerUp.initialize({
