0bc6235c75480a0294bee068c40aec775875cb3c
Serhii <32548311+SergioPSV@users.noreply.github.com>
2022-08-10 19:14:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,20 +7,20 @@ var btnCallback = function (t, opts) {
     title: 'Pull Requests',
     items: [{
       text: '#135 attempt to fix trello/api-docs#134',
-      callback: function (t, opts) {  },
+      callback: function (t, opts) { t.alert({message: 'Сохраняем тег...', duration: 10}); },
       url: 'https://github.com/trello/api-docs/pull/135'
     }, {
       text: '#133 Removing duplicate `status` property',
-      callback: function (t, opts) {  },
+      callback: function (t, opts) { t.alert({message: 'Сохраняем тег...', duration: 10}); },
       url: 'https://github.com/trello/api-docs/pull/133'
     }, {
       text: '#131 Update New Action Default',
-      callback: function (t, opts) {  }
+      callback: function (t, opts) { t.alert({message: 'Сохраняем тег...', duration: 10}); }
       url: 'https://github.com/trello/api-docs/pull/131'
     }, {
       alwaysVisible: true, // non-search option, always shown
       text: 'Choose a different repo...',
-      callback: function (t, opts) {  }
+      callback: function (t, opts) { t.alert({message: 'Сохраняем тег...', duration: 10}); }
     }],
     search: {
       count: 10, // number of items to display at a time
