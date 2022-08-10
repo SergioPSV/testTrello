96339d1b60a21352734a4acfde4cc5711f5e6ca5
Serhii <32548311+SergioPSV@users.noreply.github.com>
2022-08-10 20:44:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,31 @@ let tags = ["first", "second", "three", "four"];
 var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 
 var onBtnClick = function (t, opts) {
-  t.alert({message: 'Сохраняем тег...', duration: 2});
+  return t.popup({
+    title: 'Pull Requests',
+    items: [{
+      text: '#135 attempt to fix trello/api-docs#134',
+      callback: function (t, opts) {  },
+      url: 'https://github.com/trello/api-docs/pull/135'
+    }, {
+      text: '#133 Removing duplicate `status` property',
+      callback: function (t, opts) {  },
+      url: 'https://github.com/trello/api-docs/pull/133'
+    }, {
+      text: '#131 Update New Action Default',
+      callback: function (t, opts) {  }
+      url: 'https://github.com/trello/api-docs/pull/131'
+    }, {
+      alwaysVisible: true, // non-search option, always shown
+      text: 'Choose a different repo...',
+      callback: function (t, opts) {  }
+    }],
+    search: {
+      count: 10, // number of items to display at a time
+      placeholder: 'Search pull requests',
+      empty: 'No pull requests found'
+    }
+  });
 };
 
 window.TrelloPowerUp.initialize({
