90f902eb8b5057815f842bd2cb46402a8614c155
Serhii <32548311+SergioPSV@users.noreply.github.com>
2022-08-11 12:06:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -42,20 +42,27 @@ let tags = ["first", "second", "three", "four"];
 
 var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 
-var onBtnClick = function (t, opts) {
-  
-  const items = (_, options) => {
-    //console.log(options.search);
-    //tags.push(options.search);
+ var onBtnClick = function (t, opts) {
+//   const items = (_, options) => {
+//     //console.log(options.search);
+//     //tags.push(options.search);
     
-    tags.filter(tag =>
-      tag.toLowerCase().includes(options.search.toLowerCase()) || tag == options.search).map(tag => ({
-        alwaysVisible: tag == options.search,
+//     tags.filter(tag =>
+//       tag.toLowerCase().includes(options.search.toLowerCase()) || tag == options.search).map(tag => ({
+//         alwaysVisible: tag == options.search,
+//         text: tag,
+//         callback: t => t.alert({message: 'Сохраняем тег...', duration: 10}),
+//       })
+//     );
+//   }
+  
+    const items = (_, options) => tags.filter(tag =>
+      tag.toLowerCase().includes(options.search.toLowerCase()) || tag == 1).map(tag => ({
+        alwaysVisible: tag == 1,
         text: tag,
         callback: t => t.alert({message: 'Сохраняем тег...', duration: 10}),
-      })
-    );
-  }
+    })
+
   
   
   return t.popup({
