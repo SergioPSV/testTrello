e73afe9b35c97b584eba1d18f35cb564476f7bb2
Serhii <32548311+SergioPSV@users.noreply.github.com>
2022-08-11 12:09:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,27 @@ let tags = ["first", "second", "three", "four"];
 
 var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 
- var onBtnClick = function (t, opts) {
+const badgeClickCallback = (tee, cardId) => {
+  const items = (_, options) => tags.filter(tag =>
+    tag.toLowerCase().includes(options.search.toLowerCase()) || tag === 1).map(tag => ({
+      alwaysVisible: tag === 1,
+      text: tag,
+      callback: t => t.alert({message: 'Сохраняем тег...', duration: 10}),
+    })
+                                                                                       
+  return t.popup({
+    title: 'Теги',
+    items,
+    search: {
+      count: 10, // number of items to display at a time
+      placeholder: 'Search pull requests',
+      empty: 'Нужно добавить такой тег'
+    }
+  });                                                                                
+  );
+
+
+// var onBtnClick = function (t, opts) {
 //   const items = (_, options) => {
 //     //console.log(options.search);
 //     //tags.push(options.search);
@@ -56,32 +76,32 @@ var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4b
 //     );
 //   }
   
-    const items = (_, options) => tags.filter(tag =>
-      tag.toLowerCase().includes(options.search.toLowerCase()) || tag == 1).map(tag => ({
-        alwaysVisible: tag == 1,
-        text: tag,
-        callback: t => t.alert({message: 'Сохраняем тег...', duration: 10}),
-    })
+//     const items = (_, options) => tags.filter(tag =>
+//       tag.toLowerCase().includes(options.search.toLowerCase()) || tag == 1).map(tag => ({
+//         alwaysVisible: tag == 1,
+//         text: tag,
+//         callback: t => t.alert({message: 'Сохраняем тег...', duration: 10}),
+//     })
 
   
   
-  return t.popup({
-    title: 'Теги',
-    items,
-    search: {
-      count: 10, // number of items to display at a time
-      placeholder: 'Search pull requests',
-      empty: 'Нужно добавить такой тег'
-    }
-  });
-};
+//   return t.popup({
+//     title: 'Теги',
+//     items,
+//     search: {
+//       count: 10, // number of items to display at a time
+//       placeholder: 'Search pull requests',
+//       empty: 'Нужно добавить такой тег'
+//     }
+//   });
+// };
 
 window.TrelloPowerUp.initialize({
   'card-buttons': function (t, opts) {
     return [{
       icon: GRAY_ICON, // don't use a colored icon here
       text: 'Создать тег',
-      callback: onBtnClick,
+      callback: badgeClickCallback,
       condition: 'edit'
     }];
   }
