315457d3d5df668fe97326fbd2c6db7ba9f91d9e
Serhii <32548311+SergioPSV@users.noreply.github.com>
2022-08-11 12:15:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -42,24 +42,25 @@ let tags = ["first", "second", "three", "four"];
 
 var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 
-const badgeClickCallback = (tee, cardId) => {
-  const items = (_, options) => tags.filter(tag =>
-    tag.toLowerCase().includes(options.search.toLowerCase()) || tag === 1).map(tag => ({
-      alwaysVisible: tag === 1,
-      text: tag,
-      callback: t => t.alert({message: 'Сохраняем тег...', duration: 10}),
-    })
-                                                                                       
-  return t.popup({
-    title: 'Теги',
-    items,
-    search: {
-      count: 10, // number of items to display at a time
-      placeholder: 'Search pull requests',
-      empty: 'Нужно добавить такой тег'
-    }
-  });                                                                                
-  );
+const badgeClickCallback = (t, opts) => {
+    const items = (_, options) => tags.filter(tag =>
+        tag.toLowerCase().includes(options.search.toLowerCase()) || tag === 1).map(tag => ({
+            alwaysVisible: tag === 1,
+            text: tag,
+            callback: t => t.alert({message: 'Сохраняем тег...', duration: 10}),
+        })
+    );
+
+    return tee.popup({
+        title: 'Теги проблем',
+        items,
+        search: {
+            count: 10,
+            placeholder: 'Пошук...',
+            empty: 'Нема результатів'
+        }
+    });
+};
 
 
 // var onBtnClick = function (t, opts) {
