833f801edc55ef798b18bfd0a0a424530be1ea7c
Serhii <32548311+SergioPSV@users.noreply.github.com>
2022-08-11 12:26:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -43,13 +43,17 @@ let tags = ["first", "second", "three", "four"];
 var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 
 const badgeClickCallback = (t, opts) => {
-    const items = (_, options) => tags.filter(tag =>
-        tag.toLowerCase().includes(options.search.toLowerCase()) || tag === 1).map(tag => ({
-            alwaysVisible: tag === 1,
-            text: tag,
-            callback: t => t.alert({message: 'Сохраняем тег...', duration: 10}),
-        })
-    );
+    console.log(options.search);
+    
+    const items = (_, options) =>  {
+        tags.filter(tag =>
+            tag.toLowerCase().includes(options.search.toLowerCase()) || tag === 1).map(tag => ({
+                alwaysVisible: tag === 1,
+                text: tag,
+                callback: t => t.alert({message: 'Сохраняем тег...', duration: 10}),
+            })
+        );
+    };
 
     return t.popup({
         title: 'Теги проблем',
