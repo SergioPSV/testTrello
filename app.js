f9456fabee595bbdcdffbd7a07379bc16e094efc
Serhii <32548311+SergioPSV@users.noreply.github.com>
2022-08-11 11:32:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -62,12 +62,12 @@ var onBtnClick = function (t, opts) {
     items: [{
       alwaysVisible: false,
       text: 'test',
-      callback: console.log(items),
+      callback: t.alert({message: 'Сохраняем тег...', duration: 10}),
     },
-           {
+    {
       alwaysVisible: false,
       text: 'test2',
-      callback: console.log(items),
+      callback: t.alert({message: 'Сохраняем тег...', duration: 10}),
     }],
     search: {
       count: 10, // number of items to display at a time
@@ -80,19 +80,10 @@ var onBtnClick = function (t, opts) {
 window.TrelloPowerUp.initialize({
   'card-buttons': function (t, opts) {
     return [{
-      // usually you will provide a callback function to be run on button click
-      // we recommend that you use a popup on click generally
       icon: GRAY_ICON, // don't use a colored icon here
-      text: 'Open Popup',
+      text: 'Создать тег',
       callback: onBtnClick,
       condition: 'edit'
-    }, {
-      // but of course, you could also just kick off to a url if that's your thing
-      icon: GRAY_ICON,
-      text: 'Just a URL',
-      condition: 'always',
-      url: 'https://developer.atlassian.com/cloud/trello',
-      target: 'Trello Developer Site' // optional target for above url
     }];
   }
 });
