807b2618f5312b639ca92215d6a79b8a96421c30
Serhii <32548311+SergioPSV@users.noreply.github.com>
2022-08-12 17:51:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,73 +1,114 @@
+const GET_TAG_URL = 'https://us-central1-trello-tags.cloudfunctions.net/getTagByCard?cardId=';
+const DELETE_TAG_URL = 'https://us-central1-trello-tags.cloudfunctions.net/deleteTagCard';
+const SAVE_TAG_URL = 'https://us-central1-trello-tags.cloudfunctions.net/saveTagCard';
+const GET_TAGS_URL = 'https://us-central1-trello-tags.cloudfunctions.net/getTags';
 const CREATE_TAG = 'https://us-central1-trello-tags.cloudfunctions.net/createTag';
+const DEFAULT_TAG = 'Обрати тег';
 
+let tags = [];
+let currentTag = '';
+let currentCardId = '';
 let newTag = '';
 
+fetch(GET_TAGS_URL)
+    .then((response) => response.json())
+    .then(data => {
+        tags = data;
+        tags.unshift({ name: 'Видалити тег', id: 1 });
 
-window.TrelloPowerUp.initialize({
-  "card-detail-badges": function (t, opts) {
-    return t
-      .card("name")
-      .get("name")
-      .then(function (cardName) {
+        window.TrelloPowerUp.initialize({
+            'card-detail-badges': (t) => t.card('id').get('id').then((id) => ([{
+                dynamic: () => getTagForCard(id, t),
+            }])),
+        });
+    });
 
-        return [        
-          {
-            title: "Popup Detail Badge",
-            text: "Створити тег",
-            color: "green",
-            callback: function (t, opts) {
-                let tags = ["first", "second", "three", "four"];
-                
-                const items = (_, options) => {
-        
-                    let searchTag = tags.filter(tag =>
-                        tag.toLowerCase().includes(options.search.toLowerCase()) || tag == options.search).map(tag => ({
-                            alwaysVisible: false,
-                            text: tag,
-                            callback: t => t.alert({message: 'Сохраняем тег...', duration: 10}),
-                        })
-                    );
+const getTagForCard = (cardId, t) => new Promise(async resolve => {
+    console.log('cardId', cardId);
 
-                    if (!searchTag.length) {
-                        return [{
-                            alwaysVisible: true,
-                            text: options.search,
-                            callback: t => t.popup({
-                                                  type: 'confirm',
-                                                  title: "Створити тег?",
-                                                  message: options.search,
-                                                  confirmText: "Тааак!",
-                                                  onConfirm: t => confirmNewTag(t, options.search),
-                                                  confirmStyle: 'primary',
-                                                }),
-                        }]
-                    } else {
-                        return searchTag
-                    }
-                  }
-                
-                const confirmNewTag = async (t, tagName) => {
-                        t.alert({message: 'Зберігаю його для тебе ❤️', duration: 2})
-                  
-                        newTag = tagName;
-                        await fetch(CREATE_TAG + `?name=${newTag}`);
-                                                          
-                        t.closePopup();
-                  }
+    if (!currentTag || currentCardId !== cardId) {
+        let response = await fetch(GET_TAG_URL + cardId);
 
-                    return t.popup({
-                        title: 'Теги проблем',
-                        items,
-                        search: {
-                            count: 10,
-                            placeholder: 'Пошук...',
-                            empty: 'Цей тег потрібно створити'
-                        }
-                    });
-                
-            },
-          },       
-        ];
-      });
-  },
+        if (response.ok) {
+            const { errorCode, tagId } = await response.json();
+            currentTag = !errorCode ? tags.find(t => t.id === tagId).name : DEFAULT_TAG;
+        } else {
+            console.log("HTTP error: " + response.status);
+        }
+    }
+
+    currentCardId = cardId;
+
+    t.hideAlert()
+
+    resolve({
+        title: 'Тег проблемы',
+        text: currentTag,
+        color: "yellow",
+        refresh: 10,
+        callback: (tee) => badgeClickCallback(tee, cardId),
+    })
 });
+
+const saveTagForCard = async (tagName, cardId, t) => {
+    t.alert({message: 'Сохраняем тег...', duration: 10});
+
+    const {id:tagId} = tags.find(t => t.name === tagName);
+    if (tagId === 1) {
+        currentTag = DEFAULT_TAG;
+        await fetch(DELETE_TAG_URL + `?cardId=${cardId}`);
+    } else {
+        currentTag = tagName;
+        await fetch(SAVE_TAG_URL + `?cardId=${cardId}&tagId=${tagId}`);
+    }
+
+    t.closePopup();
+}
+
+const badgeClickCallback = (tee, cardId) => {
+    const items = (_, options) => {
+        let searchTags = tags.filter(tag =>
+            tag.name.toLowerCase().includes(options.search.toLowerCase()) || tag.id === 1).map(tag => ({
+                alwaysVisible: tag.id === 1,
+                text: tag.name,
+                callback: t => saveTagForCard(tag.name, cardId, t),
+            })
+        );
+
+        if (searchTags.length == 1) {
+            return [{
+                alwaysVisible: true,
+                text: options.search,
+                callback: t => t.popup({
+                    type: 'confirm',
+                    title: "Створити тег?",
+                    message: options.search,
+                    confirmText: "Так!",
+                    onConfirm: t => confirmNewTag(t, options.search),
+                    confirmStyle: 'primary',
+                }),
+            }]
+        } else {
+            return searchTags;
+        }
+    };
+
+    const confirmNewTag = async (t, tagName) => {
+        t.alert({message: 'Зберігаю його для тебе ❤️', duration: 2});
+
+        newTag = tagName;
+        await fetch(CREATE_TAG + `?name=${newTag}`);
+
+        t.closePopup();
+    };
+
+    return tee.popup({
+        title: 'Теги проблем',
+        items,
+        search: {
+            count: 10,
+            placeholder: 'Пошук...',
+            empty: 'Цей тег потрібно створити'
+        }
+    });
+};
