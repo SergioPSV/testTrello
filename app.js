607c887416b4bef578665a028b8b3151cedcf7ba
Serhii <32548311+SergioPSV@users.noreply.github.com>
2023-02-20 16:44:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -408,7 +408,7 @@ const btnCallbackFeedback = (t, opts) => {
     url: t.signUrl('./feedback.html', {
             args: {trello_url: 1234, trello_card_id: 456789}
           }),
-    height: 658,
+    height: 700,
   });
 };
 
